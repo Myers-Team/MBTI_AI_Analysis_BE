7f319438577a732ca1bce3dee9f9e472b870c71c
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2023-03-17 00:37:58 +0900

    fix: rename module and other

---
+++ src/main/java/com/mbtiai/demo/MbtiaitestApplication.java
@@ -0,0 +1,13 @@
+package com.mbtiai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiaitestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbtiaitestApplication.class, args);
+    }
+
+}
