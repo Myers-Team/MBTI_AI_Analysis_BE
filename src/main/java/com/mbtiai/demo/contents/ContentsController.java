620a31f2ebe8a479d3870f242af8a22a317f65e0
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2023-05-26 16:13:28 +0900

    feat: implement contents controller and service(-ing)

---
+++ src/main/java/com/mbtiai/demo/contents/ContentsController.java
@@ -0,0 +1,12 @@
+package com.mbtiai.demo.contents;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/content")
+public class ContentsController {
+
+
+}
