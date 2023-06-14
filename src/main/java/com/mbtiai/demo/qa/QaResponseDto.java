bcac3232f6016c22436383a304ae105725dc476a
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2023-06-14 20:51:05 +0900

    implement qa, pqa response, request dto

---
+++ src/main/java/com/mbtiai/demo/qa/QaResponseDto.java
@@ -0,0 +1,25 @@
+package com.mbtiai.demo.qa;
+
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class QaResponseDto {
+
+    private String question;
+    private String answer;
+    private float emd_v;
+    private Long user_id;
+
+    public static QaResponseDto fromEntity(Qa qa){
+        return QaResponseDto.builder()
+                .question(qa.getQuestion())
+                .answer(qa.getAnswer())
+                .emd_v(qa.getEmd_v())
+                .user_id(qa.getUser().getUser_id())
+                .build();
+    }
+}
