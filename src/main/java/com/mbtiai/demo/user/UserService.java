801893adf141efb403433e745f290155babf2763
kimgy12 <127962028+kimgy12@users.noreply.github.com>
2023-06-19 23:34:51 +0900

    Delete src/main/java/com/mbtiai/demo/user directory

--- src/main/java/com/mbtiai/demo/user/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.mbtiai.demo.user;
-
-import javax.transaction.Transactional;
-import java.util.Map;
-
-public interface UserService {
-    Long signUp(UserSignUpRequestDto requestDto);
-    String signin(UserSignInRequestDto requestDto);
-}
